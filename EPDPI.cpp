dbab0e55e1228300225ccc32466eed91dfca9776
Daniel <daniel.rusev@novarto.com>
2020-05-06 13:52:15 +0300

    update

--- EPDPI.cpp
+++ EPDPI.cpp
@@ -5,7 +5,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <stdexcept>
-
+#include <iostream>
 #include <stdint.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "lib/stb-master/stb_image.h"
@@ -15,11 +15,13 @@
 void SleepMs(int ms);
 void sendImage(char *filename, int cdc);
 
-unsigned char RESOLUTION[] = { 0xA8, 0x00, 0x0C, 0x01, 0x00, 0x00, 0x06, 0x40, 0x04, 0xB0, 0x00, 0x00, 0x00 };  	//Display Resolution 1600 x 1200
+unsigned char RESOLUTION[] = { 0xA8, 0x00, 0x0C, 0x01, 0x00, 0x00, 0x08, 0x98, 0x06, 0x72, 0x00, 0x00, 0x00 };  	//Display Resolution 2220 x 1650
 unsigned char VCOM[] = { 0xA8, 0x00, 0x0A, 0x03, 0x00, 0x00, 0xf8, 0xD0, 0x00, 0x00, 0x00 };              			//VCOM -1780 mV    = 0xf830 
+
 unsigned char DGREY_LAVEL[] = { 0xA8, 0x00, 0x09, 0x04, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00 };			           	//GREY_LAVEL         16Bit
 unsigned char CONTRAST[] = { 0xA8, 0x00, 0x09, 0x06, 0x00, 0x00, 0x64, 0x00, 0x00, 0x00 };					   		//CONTRAST           50
-unsigned char BUS[] = { 0xA8, 0x00, 0x09, 0x05, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00 };                    			//BUS                8Bit
+unsigned char BUS[] = { 0xA8, 0x00, 0x09, 0x05, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00 };                    			//BUS                8Bit
+
 unsigned char CLEAR_SCREEN[] = { 0xA8, 0x00, 0x09, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };                    	//CMD                CLEAR_SCREEN
 unsigned char WHITE_SCREEN[] = { 0xA8, 0x00, 0x09, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };                    	//CMD			     White screen
 unsigned char BLACK_SCREEN[] = { 0xA8, 0x00, 0x09, 0x0B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };                    	//CMD				 Black screen
@@ -27,30 +29,46 @@ unsigned char SHOW_THE_PICTURE[] = { 0xA8, 0x00, 0x09, 0x08, 0x00, 0x00, 0x01, 0
 
 
 int main(int argc, char ** argv) {
-
- 	int cdc_filestream = -1;
+	
+	int cdc_filestream = -1;
 
 	//OPEN THE UART
 	
 	cdc_filestream = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
 	
-	
-
 	if (cdc_filestream == -1)
 	{
-		//ERROR - CAN'T OPEN SERIAL PORT
-		perror("open_port: Unable to open /dev/ttyACM0 - ");
-		return(-1);
+		close(cdc_filestream);
+		fprintf(stderr, "open_port: Unable to open /dev/ttyACM0.\n");
+
+		// //ERROR - CAN'T OPEN SERIAL PORT
+		// perror("open_port: Unable to open /dev/ttyACM0 - ");
+		// return(-1);
 	}
 	
 	//CONFIGURE THE UART
 	
 	struct termios options;
 	tcgetattr(cdc_filestream, &options);
-	options.c_cflag = B1000000 | CS8 | CLOCAL | CREAD;		//<Set baud rate
-	options.c_iflag = IGNPAR;
+	// Enable the receiver and set local mode...
+	options.c_cflag |= (CLOCAL | CREAD | CS8);
+	options.c_cflag &= ~PARENB;  // ignore parity
+	options.c_cflag &= ~CSTOPB;  // 1 stop bit (2 if set)
+	options.c_cflag &= ~CSIZE;   // clear the size bits
+	options.c_cflag &= ~CRTSCTS; //No hard flow control
+	options.c_cflag &= ~HUPCL;   //Hang Up on last Close
+	options.c_cflag |= CS8;      // reset the size to 8 bits / char
+	options.c_cc[VMIN]=1;
+	options.c_cc[VTIME] = 1;
 	options.c_oflag = 0;
-	options.c_lflag = 0;
+	options.c_lflag = 0;       //ICANON;
+	// Set the new options for the port...
+	tcsetattr(cdc_filestream, TCSANOW, &options);
+
+	// options.c_cflag = 0xB1000000 | CS8 | CLOCAL | CREAD;		//<Set baud rate
+	// options.c_iflag = IGNPAR;
+	// options.c_oflag = 0;
+	// options.c_lflag = 0;
 	tcflush(cdc_filestream, TCIFLUSH);
 	tcsetattr(cdc_filestream, TCSANOW, &options);
 		
@@ -75,50 +93,16 @@ int main(int argc, char ** argv) {
 
 		SleepMs(500);
 
-		//sendImage(argv[1], cdc_filestream);
-		//SleepMs(10);
-		//int n = write(cdc_filestream, SHOW_THE_PICTURE, 0x09);
-				
-		//sendImage("/home/pi/Desktop/1280x800/1.jpg", cdc_filestream);
-				
-		sendImage( (char*)("1.jpg"), cdc_filestream);  // Open file 
+		sendImage(argv[1], cdc_filestream);
 		SleepMs(10);
-	int n = write(cdc_filestream, SHOW_THE_PICTURE, 0x09);
-		SleepMs(2000);
-
-		sendImage((char*)("2.jpg"), cdc_filestream);  // Open file 
-		SleepMs(10);
-	    n = write(cdc_filestream, SHOW_THE_PICTURE, 0x09);
+		int n = write(cdc_filestream, SHOW_THE_PICTURE, 0x09);
 		SleepMs(2000);
 		
-		sendImage((char*)("3.jpg"), cdc_filestream);  // Open file 
-		SleepMs(10);
-		n = write(cdc_filestream, SHOW_THE_PICTURE, 0x09);
-		SleepMs(2000);
-						
-		sendImage((char*)("4.jpg"), cdc_filestream);  // Open file 
-		SleepMs(10);
-		n = write(cdc_filestream, SHOW_THE_PICTURE, 0x09);
-		SleepMs(2000);
-		
-		
-		SleepMs(500);
-
-		n = write(cdc_filestream, CLEAR_SCREEN, 0x09);
-		SleepMs(500);
-
-				
-		n = write(cdc_filestream, BLACK_SCREEN, 0x09);
-		SleepMs(500);
-
-		
-		n = write(cdc_filestream, WHITE_SCREEN, 0x09);
-		SleepMs(500);
-
-	
 	if (n < 0) {
-		perror("Write failed - ");
-		return -1;
+		close(cdc_filestream);
+		fprintf(stderr, "Write failed.\n");
+		// perror("Write failed - ");
+		return 0;
 	}
 
 
