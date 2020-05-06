265eae4311cc7012797f21e90268cd7c1e962e68
Daniel <daniel.rusev@novarto.com>
2020-05-06 13:18:25 +0300

    update

--- EPDPI.cpp
+++ EPDPI.cpp
@@ -16,7 +16,7 @@ void SleepMs(int ms);
 void sendImage(char *filename, int cdc);
 
 unsigned char RESOLUTION[] = { 0xA8, 0x00, 0x0C, 0x01, 0x00, 0x00, 0x08, 0x98, 0x06, 0x72, 0x00, 0x00, 0x00 };  	//Display Resolution 1600 x 1200
-unsigned char VCOM[] = { 0xA8, 0x00, 0x0A, 0x03, 0x00, 0x00, 0xf9, 0x0C, 0x00, 0x00, 0x00 };              			//VCOM -1780 mV    = 0xf830 
+unsigned char VCOM[] = { 0xA8, 0x00, 0x0A, 0x03, 0x00, 0x00, 0xf8, 0xD0, 0x00, 0x00, 0x00 };              			//VCOM -1780 mV    = 0xf830 
 unsigned char DGREY_LAVEL[] = { 0xA8, 0x00, 0x09, 0x04, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00 };			           	//GREY_LAVEL         16Bit
 unsigned char CONTRAST[] = { 0xA8, 0x00, 0x09, 0x06, 0x00, 0x00, 0x00, 0x64, 0x00, 0x00 };					   		//CONTRAST           50
 unsigned char BUS[] = { 0xA8, 0x00, 0x09, 0x05, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00 };                    			//BUS                8Bit
@@ -47,12 +47,13 @@ int main(int argc, char ** argv) {
 	
 	struct termios options;
 	tcgetattr(cdc_filestream, &options);
-	options.c_cflag = B1000000 | CS8 | CLOCAL | CREAD;		//<Set baud rate
+	options.c_cflag = CS8 | CLOCAL | CREAD;		//<Set baud rate
 	options.c_iflag = IGNPAR;
 	options.c_oflag = 0;
 	options.c_lflag = 0;
 	tcflush(cdc_filestream, TCIFLUSH);
 	tcsetattr(cdc_filestream, TCSANOW, &options);
+	cfsetispeed(&options, B115200);
 
 	// Turn off blocking for reads, use (fd, F_SETFL, FNDELAY) if you want that
 	fcntl(cdc_filestream, F_SETFL, 0);
